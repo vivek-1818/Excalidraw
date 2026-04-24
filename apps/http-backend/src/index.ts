ae1390203c43ed1daa8c43a5e5faa98b84fe7138
Vivek Biradar <vivekbiradar1818@gmail.com>
2026-04-24 23:14:32 +0530

    added signup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -7,15 +7,30 @@ import { prismaClient } from "@repo/db/client"
 
 const app = express();
 
-app.post("/signup", (req,res)=>{
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signup", async (req,res)=>{
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
         res.json({
             message: "Incorrect Inputs"
         })
         return
     }
-    // db call
+    
+    try {
+        await prismaClient.user.create({
+            data:{
+                email: parsedData.data?.username,
+                password: parsedData.data?.password,
+                name: parsedData.data?.name
+            }
+        })    
+    } catch (e) {
+        res.status(411).json({
+            message: "User already exist with this username"
+        })
+    }
+    
+    
     res.json({
         userId: "123"
     })
