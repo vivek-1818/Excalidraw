3b006e0aa4ac23108a758a03e2d05fd94f1a41de
Vivek Biradar <vivekbiradar1818@gmail.com>
2026-05-11 19:46:06 +0530

    fix monorepo git tracking

--- packages/db/dist/generated/prisma/models.d.ts
+++
@@ -1,6 +0,0 @@
-export type * from './models/User.js';
-export type * from './models/Room.js';
-export type * from './models/Chat.js';
-export type * from './models/CanvasImage.js';
-export type * from './commonInputTypes.js';
-//# sourceMappingURL=models.d.ts.map
--- packages/db/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const prismaClient: import("./generated/prisma/internal/class.js").PrismaClient<never, import("./generated/prisma/internal/prismaNamespace.js").GlobalOmitConfig | undefined, import("@prisma/client/runtime/client").DefaultArgs>;
-//# sourceMappingURL=index.d.ts.map
