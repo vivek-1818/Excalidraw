7586944cbd7dde450dd43ec57cab0b23a87ca78e
Vivek Biradar <vivekbiradar1818@gmail.com>
2026-05-11 17:08:43 +0530

    Fix workspace package exports for Render

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction,Request,Response } from "express";
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "../../../packages/backend-common/dist/index.js";
+import { JWT_SECRET } from "@repo/backend-common";
 
 export function middleware(req:Request, res:Response, next:NextFunction){
     const token = req.headers["authorization"] ?? "";
